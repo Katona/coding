f965eb1330de57c9bda7e0c1dc7a569d3c1daf62
Gabor Hornyak <gabhornyak@gmail.com>
2021-10-31 00:30:19 +0200

    n*log(n)

--- src/leetcode/count-of-range-sum/Solution.test.ts
+++ src/leetcode/count-of-range-sum/Solution.test.ts
@@ -1,8 +1,24 @@
-import { countRangeSum } from "./Solution"
+import { countRangeSum, countRangeSum2, findIndex } from "./Solution";
 
 describe("countRangeSum", () => {
-    it("should work.", () => {
-        expect(countRangeSum([-2, 5, -1], -2, 2)).toBe(3);
-        expect(countRangeSum([0], 0, 0)).toBe(1);
-    })
-})
+  it("should work.", () => {
+    expect(countRangeSum2([-2, 5, -1], -2, 2)).toBe(3);
+    expect(countRangeSum2([0], 0, 0)).toBe(1);
+    expect(countRangeSum2([1, 2], 1, 3)).toBe(3);
+    expect(countRangeSum2([1, 2, 3], 3, 5)).toBe(3);
+    expect(countRangeSum2([1, 2, 3], 4, 4)).toBe(0);
+    expect(countRangeSum2([1, 2, 3], 3, 6)).toBe(4);
+    expect(countRangeSum2([-3,-3,1,1,2], 3, 5)).toBe(2);
+    expect(countRangeSum2([0, 1,1,2], 3, 5)).toBe(2);
+  });
+});
+
+describe("findIndex", () => {
+  it("should work", () => {
+    expect(findIndex([0], 0, 0, 0)).toBe(0);
+    expect(findIndex([1, 2], 2, 0, 1)).toBe(1);
+    expect(findIndex([1, 2], 1, 0, 1)).toBe(0);
+    expect(findIndex([1, 2], 3, 0, 1)).toBe(2);
+    expect(findIndex([1, 2, 3], 2, 0, 2)).toBe(1);
+  });
+});
--- src/leetcode/count-of-range-sum/Solution.ts
+++ src/leetcode/count-of-range-sum/Solution.ts
@@ -1,3 +1,5 @@
+import { mergeSort } from "../global-and-local-inversions/Solution";
+
 export function countRangeSum(nums: number[], lower: number, upper: number): number {
   let prefixArray = nums.reduce<number[]>(
     (prev, curr) => [...prev, (prev[prev.length - 1] ?? 0) + curr],
@@ -11,9 +13,105 @@ export function countRangeSum(nums: number[], lower: number, upper: number): num
       const right = prefixArray[j];
       const rangeSum = right - left;
       if (rangeSum >= lower && rangeSum <= upper) {
-          result += 1;
+        result += 1;
       }
     }
   }
   return result;
 }
+type Range = {
+  start: number;
+  end: number;
+};
+
+const countRangeSums = (prefixSum: number[], left: Range, right: Range, sumRange: Range) => {
+  let i = left.start;
+  let l = right.start;
+  let r = right.start;
+  let count = 0;
+  while (i <= left.end) {
+    while (l <= right.end && prefixSum[l] - prefixSum[i] < sumRange.start) l += 1;
+    while (r <= right.end && prefixSum[r] - prefixSum[i] <= sumRange.end) r += 1;
+    count += r - l;
+    i += 1;
+  }
+  return count;
+};
+
+function arraycopy<T>(src: T[], srcPos: number, dst: T[], dstPos: number, length: number) {
+  while (length--) dst[dstPos++] = src[srcPos++];
+  return dst;
+}
+
+const merge = (nums: number[], left: Range, right: Range, buffer: number[]) => {
+  let l = left.start;
+  let r = right.start;
+  let merged = left.start;
+
+  while (merged <= right.end) {
+    if (r > right.end || l <= left.end && nums[l] < nums[r]) {
+      buffer[merged] = nums[l];
+      l += 1;
+    } else {
+      buffer[merged] = nums[r];
+      r += 1;
+    }
+    merged += 1;
+  }
+  arraycopy(buffer, left.start, nums, left.start, right.end - left.start + 1);
+};
+
+const countRangeSumSub = (
+  prefixSum: number[],
+  range: Range,
+  sumRange: Range,
+  buffer: number[]
+): number => {
+  if (range.start === range.end)
+    return prefixSum[range.start] >= sumRange.start && prefixSum[range.start] <= sumRange.end
+      ? 1
+      : 0;
+  const middle = (range.start + range.end) >> 1;
+  const leftHalf = { start: range.start, end: middle };
+  const rightHalf = { start: middle + 1, end: range.end };
+  const result =
+    countRangeSumSub(prefixSum, leftHalf, sumRange, buffer) +
+    countRangeSumSub(prefixSum, rightHalf, sumRange, buffer) +
+    countRangeSums(prefixSum, leftHalf, rightHalf, sumRange);
+  merge(prefixSum, leftHalf, rightHalf, buffer);
+  return result;
+};
+
+/**
+ * nums = [-2, 5, -1]
+ * prefix = [ 2, 3, 2]
+ */
+export function countRangeSum2(nums: number[], lower: number, upper: number): number {
+  const prefixArray = nums.reduce<number[]>(
+    (prev, curr) => [...prev, (prev[prev.length - 1] ?? 0) + curr],
+    []
+  );
+  return countRangeSumSub(
+    prefixArray,
+    { start: 0, end: prefixArray.length - 1 },
+    { start: lower, end: upper },
+    Array(nums.length - 1)
+  );
+}
+
+/**
+ * nums = [1, 2];
+ * value = 2;
+ *
+ */
+export const findIndex = (nums: number[], value: number, start: number, end: number) => {
+  let l = start;
+  let r = end;
+
+  while (l <= r) {
+    const m = (l + r) >> 1;
+    if (value <= nums[m]) r = m - 1;
+    else l = m + 1;
+  }
+  return l;
+};
